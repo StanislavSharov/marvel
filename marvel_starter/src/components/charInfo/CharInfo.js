1c29e621463c8f4411b0f519cd1ee76be9e3a0cb
Stanislav Sharov <sharon.stanislav19@gmail.com>
2025-07-26 01:29:15 +0200

    THe middle

---
+++ marvel_starter/src/components/charInfo/CharInfo.js
@@ -0,0 +1,127 @@
+import "./charInfo.scss";
+import { Component } from "react";
+import MarvelServices from "../../services/MarvelServices";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Skeleton from "../skeleton/Skeleton";
+
+class CharInfo extends Component {
+  state = {
+    char: null,
+    loading: false,
+    error: false,
+  };
+  marvelServices = new MarvelServices();
+
+  componentDidMount() {
+    this.updateChar();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.charId !== prevProps.charId) {
+      this.updateChar();
+    }
+  }
+
+  updateChar = () => {
+    const { charId } = this.props;
+    if (!charId) {
+      return;
+    }
+
+    this.onCharLoading();
+
+    this.marvelServices
+      .getCharacter(charId)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
+  };
+
+  onCharLoaded = (char) => {
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+  onCharLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
+  render() {
+    const { char, loading, error } = this.state;
+
+    const skeleton = char || loading || error ? null : <Skeleton />;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+    return (
+      <div className="char__info">
+        {skeleton}
+        {errorMessage}
+        {spinner}
+        {content}
+      </div>
+    );
+  }
+}
+
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki, comics } = char;
+  return (
+    <>
+      <div className="char__basics">
+        <img src={thumbnail} alt={name} />
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">{description}</div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length === 0 ? (
+          <li className="char__comics-item">No comics with this personage</li>
+        ) : (
+          comics.slice(0, 10).map((item, i) => (
+            <li key={i} className="char__comics-item">
+              {item.name}
+            </li>
+          ))
+        )}
+      </ul>
+
+      <ul className="char__comics-list"></ul>
+    </>
+  );
+};
+    
+export default CharInfo;
+
+/* {
+                 comics.length === 0? comics.slice(0, 10).map((item,i)=>{
+                         (
+                            <li key = {i} className="char__comics-item">
+                                {item.name }
+                            </li>
+                        )
+                    }):
+                             (
+                            <li className="char__comics-item">
+                                No comics with this personage
+                            </li>
+                        )
+                }        */
---
+++ marvel_starter/src/services/MarvelServices.js
@@ -0,0 +1,74 @@
+// const postData = async (url,data) =>{
+//     let res = await fetch(url,{
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: data
+//     });
+
+//     return await res.json();
+
+// }
+
+// async function getResources(url) {
+//     let res = await fetch(url);
+
+//     if(!res.ok){
+//         throw new Error(Could'n fetch ${url}, status ${res.status});
+
+//     }
+
+//     return await res.json();
+// }
+
+// export {postData}
+// export {getResources}
+
+class MarvelServices {
+  _apiBase = "https://marvel-server-zeta.vercel.app/";
+
+  _apiKey = "apikey=d4eecb0c66dedbfae4eab45d312fc1df";
+  getResources = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could'n fetch ${url}, status ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = async () => {
+    const res = await this.getResources(
+      "https://marvel-server-zeta.vercel.app/characters?apikey=d4eecb0c66dedbfae4eab45d312fc1df"
+    );
+    return res.data.results.map(this._transformCharacter);
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResources(`
+      https://marvel-server-zeta.vercel.app/characters/${id}?apikey=d4eecb0c66dedbfae4eab45d312fc1df`);
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+getFirstNineCharacter = async () => {
+  const res = await this.getAllCharacters();
+  return res.slice(0, 9); 
+};
+
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
+    }
+}
+
+export default MarvelServices;
